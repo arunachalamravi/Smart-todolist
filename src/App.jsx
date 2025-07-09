624a7ccc3d620c83fa6fb4e84e39083ed6974454
Arunachalam <arunachalam@crayond.co>
2025-07-09 21:33:27 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import SmartTodoList from './components/todoList'
+
+function App() {
+
+  return (
+    <div className="min-h-screen  mx-[auto]  bg-aliceblue">
+      {/* <div className="text-center"> */}
+       <SmartTodoList/>        
+      {/* </div>s */}
+    </div>
+  )
+}
+
+export default App
